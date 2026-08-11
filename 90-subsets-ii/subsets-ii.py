da5248cb9e2d86c87716bd5f3195fd2c781e2ae6
Joshua Wallace <156718075+Pikmin2001@users.noreply.github.com>
2026-08-11 18:53:14 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (22.92%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -4,14 +4,16 @@ class Solution:
         res = []
         def dfs(i, cur):
             if i == len(nums):
-                res.append(cur[::])
+                res.append(cur.copy())
                 return
+
             cur.append(nums[i])
             dfs(i+1, cur)
             cur.pop()
-            
-            while i+1 < len(nums) and nums[i] == nums[i+1]:
+            while i+1 < len(nums) and nums[i]==nums[i+1]:
                 i += 1
             dfs(i+1, cur)
+
         dfs(0, [])
         return res
+        
