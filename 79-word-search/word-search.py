58ccf10e7be34e823dbbe20ca057181338b4ef9d
Joshua Wallace <156718075+Pikmin2001@users.noreply.github.com>
2026-08-14 21:07:05 -0400

    Time: 4122 ms (40.71%) | Memory: 19.6 MB (27.04%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,29 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
+        res = []
+        path = set()
         rows = len(board)
         cols = len(board[0])
-        path = set()
-        # i = current letter of word we are searching for
+
         def dfs(r, c, i):
-            #win con
             if i == len(word):
                 return True
-            
-            if r < 0 or c < 0  or r >= rows or c >= cols or board[r][c] !=  word[i] or (r, c) in path:
+
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != word[i] or (r, c) in path:
                 return False
-                #check surrounding for word[i+1]
+
             path.add((r, c))
-            res = (dfs(r+1, c, i+1) or  
-            dfs(r, c+1, i+1) or
-            dfs(r-1, c, i+1) or
-            dfs(r, c-1, i+1))
-            path.remove((r,c))
+            res = dfs(r+1, c, i+1) or dfs(r, c+1, i+1) or dfs(r-1, c, i+1) or dfs(r, c-1, i+1) 
+            path.remove((r, c)) 
+
             return res
 
-        for j in range(rows):
-            for k in range(cols):
-                if dfs(j, k, 0) == True:
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0) == True:
                     return True
         return False
-        
