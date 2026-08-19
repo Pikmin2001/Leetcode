099e73463b7e23a191505fc0fed09e6bd21da878
Joshua Wallace <156718075+Pikmin2001@users.noreply.github.com>
2026-08-19 00:52:01 -0400

    Time: 273 ms (15.56%) | Memory: 26.7 MB (21.44%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,7 +1,6 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        
 
         rows, cols = len(grid), len(grid[0])
         islands = 0
